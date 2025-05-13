bd15b1c77ea7b289592e024140aa6950c3a38e12
Ralpmike <ralphmike996@gmail.com>
2025-05-13 13:30:31 +0100

    guessing game initial commit 1

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
 'use strict';
 
+const randomNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 const check = document.querySelector('.check');
-const message = document.querySelector('.message');
+const randomNumberDisplay = (document.querySelector('.number').textContent =
+  randomNumber);
+let chances = document.querySelector('.score');
+let message = document.querySelector('.message');
 const guessField = document.querySelector('.guessField');
 const guessSubmit = document.querySelector('.guessSubmit');
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
-const resetButton = document.querySelector('.reset');
+const playAgain = document.querySelector('.again');
 const guessCount = document.querySelector('.guessCount');
+
+check.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    message.textContent = '⛔ No number!';
+    // score--;
+    // chances.textContent = score;
+  } else if (guess > randomNumber) {
+    if (score > 1) {
+      message.textContent = '📈 Too high!';
+      score--;
+      chances.textContent = score;
+    } else {
+      message.textContent = '💥Game over!';
+      chances.textContent = 0;
+      // check.disabled = true;
+    }
+  } else if (guess > 20 || guess < 1) {
+    message.textContent = '⛔ Number out of range!';
+    score--;
+    chances.textContent = score;
+  } else if (guess < randomNumber) {
+    message.textContent = '📉 Too low!';
+    score--;
+    chances.textContent = score;
+  } else if (guess === randomNumber) {
+    message.textContent = '🎉 Correct number!';
+    // lastResult.textContent = `🎉 Correct number! ${randomNumber}`;
+    // lowOrHi.textContent = '';
+    // check.disabled = true;
+  }
+  // if (score < 1) {
+  //   message.textContent = '💥Game over!';
+  //   chances.textContent = 0;
+  //   check.disabled = true;
+  // }
+  // guessField.value = '';
+  // guessField.focus();
+  // guessCount.textContent = `Guess count: ${guess}`;
+});
+
+// playAgain.addEventListener('click', function () {
+//   score = 20;
+//   chances.textContent = score;
+//   message.textContent = 'Start guessing...';
+//   randomNumberDisplay.textContent = '?';
+//   check.disabled = false;
+//   guess.value = '';
+//   guess.focus();
+//   // lastResult.textContent = '';
+//   // lowOrHi.textContent = '';
+// });
