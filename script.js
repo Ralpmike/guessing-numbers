31172a31f8179d6c1747c6305e75615aeddb3675
Ralpmike <ralphmike996@gmail.com>
2025-05-13 11:09:41 +0100

    guessing game initial commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const check = document.querySelector('.check');
+const message = document.querySelector('.message');
+const guessField = document.querySelector('.guessField');
+const guessSubmit = document.querySelector('.guessSubmit');
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const resetButton = document.querySelector('.reset');
+const guessCount = document.querySelector('.guessCount');
